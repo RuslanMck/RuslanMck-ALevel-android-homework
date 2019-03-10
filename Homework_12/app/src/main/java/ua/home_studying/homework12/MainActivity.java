596ab9322afb4748692af039cd5cebe0a2178061
Ruslan <mckruslan@gmail.com>
2019-03-10 19:42:29 +0200

    Added two textView, button and imageView. Implemented custom onClickListener.

--- Homework_12/app/src/main/java/ua/home_studying/homework12/MainActivity.java
+++ Homework_12/app/src/main/java/ua/home_studying/homework12/MainActivity.java
@@ -2,6 +2,9 @@ package ua.home_studying.homework12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView bottomTV = findViewById(R.id.bottomTV);
+        bottomTV.setText("Hardcoded text");
+
+        final TextView topTV = findViewById(R.id.topTV);
+
+        Button button = findViewById(R.id.button);
+
+        View.OnClickListener customListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                topTV.setText(R.string.good_day);
+            }
+        };
+        button.setOnClickListener(customListener);
     }
 }
