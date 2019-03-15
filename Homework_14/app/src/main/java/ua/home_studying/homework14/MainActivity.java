a3f242e3e9b4d1cdad1dd6b17027760b43bb2668
Ruslan <mckruslan@gmail.com>
2019-03-15 16:23:16 +0200

    App is working

--- Homework_14/app/src/main/java/ua/home_studying/homework14/MainActivity.java
+++ Homework_14/app/src/main/java/ua/home_studying/homework14/MainActivity.java
@@ -3,18 +3,20 @@ package ua.home_studying.homework14;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Counter counter = new Counter();
+    ImagesList imagesList = new ImagesList();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagesList.collectImages();
 
         final TextView textViewLeft = findViewById(R.id.text_view_left);
         textViewLeft.setText(String.valueOf(counter.getCountLeft()));
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         ImageButton buttonL = findViewById(R.id.button_l);
         ImageButton buttonR = findViewById(R.id.button_r);
 
+        final ImageView imageView = findViewById(R.id.image_view);
+        imageView.setImageResource(R.drawable.nature_pic_001);
+
         View.OnClickListener customCL = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.button_l:
                         counter.countLeft();
                         textViewLeft.setText(String.valueOf(counter.getCountLeft()));
+                        imageView.setImageResource(imagesList.getPrevImg());
                         break;
                     case R.id.button_r:
                         counter.contRight();
                         textViewRight.setText(String.valueOf(counter.getCountRight()));
+                        imageView.setImageResource(imagesList.getNextImg());
                         break;
                 }
             }
@@ -46,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             textViewRight.setText(savedInstanceState.getString("SavedRightString"));
             counter.setCountLeft(savedInstanceState.getInt("SavedLeftInt"));
             counter.setCountRight(savedInstanceState.getInt("SavedRightInt"));
+            imageView.setImageResource(imagesList.getImg(savedInstanceState.getInt("SavedImage")));
         }
         buttonL.setOnClickListener(customCL);
         buttonR.setOnClickListener(customCL);
@@ -58,5 +66,6 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("SavedRightInt", counter.getCountRight());
         outState.putString("SavedLeftString", String.valueOf(counter.getCountLeft()));
         outState.putString("SavedRightString", String.valueOf(counter.getCountRight()));
+        outState.putInt("SavedImage", imagesList.getIndex());
     }
 }
